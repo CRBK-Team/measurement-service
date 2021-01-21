af7bfaebdb5e195106fd5376f9e34bf6a76f45db
Crovitz <maciej.krowicki@gmail.com>
2021-01-21 13:07:21 +0100

    Corrected entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,5 @@
 if [ ! -z "$CREDENTIALS_FILE" ] && [ -r "/run/secrets/$CREDENTIALS_FILE" ]; then
 	. /run/secrets/$CREDENTIALS_FILE
 fi
+
+java -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar /app.jar
